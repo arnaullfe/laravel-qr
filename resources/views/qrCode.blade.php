979316821c82289f0e2234b5a866014b4127722e
arnaullfe <arnaullfe@gmail.com>
2020-09-19 01:13:45 +0200

    first commit

---
+++ resources/views/qrCode.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>How to Generate QR Code in Laravel 6? - HDTuto.com</title>
+
+</head>
+
+<body>
+
+    
+
+<div class="visible-print text-center">
+
+    <h1>Laravel 6 - QR Code Generator Example</h1>
+
+     
+
+    {!! QrCode::size(250)->generate('www.google.com'); !!} 
+
+
+     
+
+    <p>example by ItSolutionStuf.com.</p>
+
+</div>
+
+    
+
+</body>
+
+</html>
